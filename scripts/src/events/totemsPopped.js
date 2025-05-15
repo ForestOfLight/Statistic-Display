889674196330c90c0ab7d81d0f36594aa268c57d
ForestOfLight <briancassriel@gmail.com>
2025-05-15 12:32:39 -0700

    Fix totemPopped event

--- scripts/src/events/totemsPopped.js
+++ scripts/src/events/totemsPopped.js
@@ -1,12 +1,12 @@
-import { Player, world } from "@minecraft/server";
+import { world } from "@minecraft/server";
 import eventManager from "src/classes/EventManager";
 
 const IDENTIFIER = 'totemsPopped';
 
 eventManager.registerEvent(IDENTIFIER, 'Totems Popped', () => {
     world.afterEvents.entityHurt.subscribe((event) => {
-        const entity = event.entity;
-        if (!entity || !(entity instanceof Player))
+        const entity = event.hurtEntity;
+        if (!entity || entity.typeId !== 'minecraft:player')
             return;
         if (hasPoppedTotem(event.damage, event.damageSource))
             eventManager.increment(IDENTIFIER, entity);
