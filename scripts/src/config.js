9f2d2bede40ff4db3d5c3ed9a158d68dec406d41
ForestOfLight <briancassriel@gmail.com>
2025-06-19 22:23:25 -0700

    bump pack and mc versions

--- scripts/src/config.js
+++ scripts/src/config.js
@@ -3,7 +3,7 @@ import { CanopyExtension } from 'lib/canopy/CanopyExtension';
 const extension = new CanopyExtension({
     name: 'StatisticDisplay',
     description: 'Statistic tracking for §l§aCanopy§r.',
-    version: '1.1.7'
+    version: '1.1.8'
 });
 
 export { extension };
